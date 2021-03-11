7d345502d3b2577b04bafc98f38aa98d466b2334
Fernando Diaz <nandodiazz23@gmail.com>
2021-03-11 09:45:25 -0500

    ensayo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=5;
+    float b=2.0;
+    char c = 'A';
+    cout << "Hello World!" << endl;
+    cout << a << endl;
+    cout << a << sizeof(a) <<endl;
+    cout << a+1 << sizeof(a) <<endl;
+    return 0;
+}
